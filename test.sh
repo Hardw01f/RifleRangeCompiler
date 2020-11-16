952e6ff82f9ba6bab50c8a22a4560b91e08e25a5
Hardwolf <hardwolf@Hardwolfs-MacBook-Pro.local>
2020-11-17 00:35:04 +0900

    test: 💍 add tests, but not yet passed

--- test.sh
+++ test.sh
@@ -21,7 +21,12 @@ assert() {
 
 assert 0 0 
 assert 42 42
-assert 150 "100+200-200+100-50"
-assert 60 " 100 + 20 + 30 - 20 - 70"
+assert 21 '5+20-4'
+assert 41 '12+34-5'
+#assert 150 '100+200-200+100-50'
+#assert 60 ' 100 + 20 + 30 - 20 - 70'
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 
 echo "OK : Test ALL Passed"
